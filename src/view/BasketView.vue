41975fe3824783a26231a089553bef146921d79c
kirill-02 <136984088+kirill-02@users.noreply.github.com>
2025-01-16 19:17:39 +0400

    update Bastet.vue

--- src/view/BasketView.vue
+++ src/view/BasketView.vue
@@ -1,17 +1,141 @@
 <template>
-<div class="basket">
-  <div class="container">
-  <div class="basket__wrapper">
+  <div class="basket">
+    <div class="container">
+      <div class="title">Ваша корзина</div>
+      <div class="description">3 товара / 1030 руб.</div>
 
+
+      <div class="basket__wrapper">
+        <div class="basket__wrapper__information">
+
+          <div class="basket__wrapper__information__product">
+
+            <div class="basket__wrapper__information__product__layout">
+              <div class="basket__wrapper__information__product__layout_images"><img
+                  src="../../public/img/home/5/1.jpeg" alt=""></div>
+              <div class="basket__wrapper__information__product__layout__things">
+                <div class="basket__wrapper__information__product__layout__things_title"><strong>Набор макарон 12
+                  шт.</strong>
+                </div>
+                <div class="basket__wrapper__information__product__layout__things__description">
+                  <div class="">Трюфель 2 шт.</div>
+                  <div class="">Розмарин 3 шт.</div>
+                  <div class=""> Ананас 1 шт.</div>
+                  <div class="">Кокос 4 шт.</div>
+                </div>
+              </div>
+
+              <div class="basket__wrapper__information__product__layout__quantity">
+                <button class="basket__wrapper__information__product__layout__quantity_minus">-</button>
+                <div class="basket__wrapper__information__product__layout__quantity_number">1</div>
+                <button class="basket__wrapper__information__product__layout__quantity_plus">+</button>
+              </div>
+
+              <div class="basket__wrapper__information__product__layout__price">
+                <div> Цена: <span>950 руб.</span></div>
+              </div>
+            </div>
+            <hr>
+            <div class="basket__wrapper__information__product__layout">
+              <div class="basket__wrapper__information__product__layout_images"><img
+                  src="../../public/img/home/5/1.jpeg" alt=""></div>
+              <div class="basket__wrapper__information__product__layout__things">
+                <div class="basket__wrapper__information__product__layout__things_title"><strong>Набор макарон 12
+                  шт.</strong></div>
+                <div class="basket__wrapper__information__product__layout__things__description">
+                  <div class="">Трюфель 2 шт.</div>
+                  <div class="">Розмарин 3 шт.</div>
+                  <div class=""> Ананас 1 шт.</div>
+                  <div class="">Кокос 4 шт.</div>
+                </div>
+              </div>
+
+              <div class="basket__wrapper__information__product__layout__quantity">
+                <button class="basket__wrapper__information__product__layout__quantity_minus">-</button>
+                <div class="basket__wrapper__information__product__layout__quantity_number">1</div>
+                <button class="basket__wrapper__information__product__layout__quantity_plus">+</button>
+              </div>
+
+              <div class="basket__wrapper__information__product__layout__price">
+                <div> Цена: <span>950 руб.</span></div>
+              </div>
+            </div>
+            <hr>
+            <div class="basket__wrapper__information__product__layout">
+              <div class="basket__wrapper__information__product__layout_images"><img
+                  src="../../public/img/home/5/1.jpeg" alt=""></div>
+              <div class="basket__wrapper__information__product__layout__things">
+                <div class="basket__wrapper__information__product__layout__things_title"><strong>Набор макарон 12
+                  шт.</strong></div>
+                <div class="basket__wrapper__information__product__layout__things__description">
+                  <div class="">Трюфель 2 шт.</div>
+                  <div class="">Розмарин 3 шт.</div>
+                  <div class=""> Ананас 1 шт.</div>
+                  <div class="">Кокос 4 шт.</div>
+                </div>
+              </div>
+
+              <div class="basket__wrapper__information__product__layout__quantity">
+                <button class="basket__wrapper__information__product__layout__quantity_minus">-</button>
+                <div class="basket__wrapper__information__product__layout__quantity_number">1</div>
+                <button class="basket__wrapper__information__product__layout__quantity_plus">+</button>
+              </div>
+
+              <div class="basket__wrapper__information__product__layout__price">
+                <div> Цена: <span>950 руб.</span></div>
+              </div>
+            </div>
+            <hr>
+            <div class="basket__wrapper__information__product__total-price">
+              <div class="basket__wrapper__information__product__total-price_description">Общая стоимость Вашей покупки
+                без учета доставки составит:
+              </div>
+              <div class="basket__wrapper__information__product__total-price_price">1000 руб.</div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="basket__wrapper__result">
+          <div class="basket__wrapper__result_title">Итого</div>
+
+          <div class="basket__wrapper__result__information">
+            <div class="basket__wrapper__result__information__card">
+              <div class="basket__wrapper__result__information__card_title">Стоимость товаров</div>
+              <div class="basket__wrapper__result__information__card_dot-one"></div>
+              <div class="basket__wrapper__result__information__card_price">700 руб</div>
+            </div>
+            <div class="basket__wrapper__result__information__card">
+              <div class="basket__wrapper__result__information__card_title">Скидка</div>
+              <div class="basket__wrapper__result__information__card_dot-two"></div>
+              <div class="basket__wrapper__result__information__card_price">0 руб</div>
+            </div>
+            <div class="basket__wrapper__result__information__card">
+              <div class="basket__wrapper__result__information__card_title">Доставка</div>
+              <div class="basket__wrapper__result__information__card_dot-three"></div>
+              <div class="basket__wrapper__result__information__card_price">400 руб</div>
+            </div>
+            <hr>
+            <div class="basket__wrapper__result__information__card">
+              <div class="basket__wrapper__result__information__card_title">К оплате</div>
+              <div class="basket__wrapper__result__information__card_price"><strong> 1100 руб</strong></div>
+            </div>
+          </div>
+          <hr>
+
+          <button class="basket__wrapper__result_btn">Оформить заказ</button>
+        </div>
+      </div>
+
+
+    </div>
   </div>
-  </div>
-</div>
 </template>
 
-<script >
+<script>
 
 </script>
 
-<style scoped>
+<style src="@/assets/style/Basket.css" scoped>
 
 </style>
