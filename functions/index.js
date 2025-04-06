d322a157c46bcef3c8d5d7203f7451fb425911da
kirill-02 <136984088+kirill-02@users.noreply.github.com>
2025-04-06 22:06:34 +0400

    update firebase

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,52 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express();
+app.use(cors());
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Настройка хранения файлов
+const uploadDir = '/tmp'; // Временная директория для хранения загруженных файлов
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = uuidv4() + path.extname(file.originalname);
+        cb(null, uniqueName);
+    },
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Инициализация multer
+const upload = multer({ storage: storage });
+
+// Корневой маршрут
+app.get('/', (req, res) => {
+    res.send('Welcome to the image upload server!');
+});
+
+// Маршрут для загрузки файла
+app.post('/upload', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }]), (req, res) => {
+    if (!req.files) {
+        return res.status(400).json({ error: 'No files uploaded' });
+    }
+
+    const filePaths = [];
+    if (req.files['image']) {
+        filePaths.push(`${req.files['image'][0].filename}`);
+    }
+    if (req.files['image2']) {
+        filePaths.push(`${req.files['image2'][0].filename}`);
+    }
+    if (req.files['image3']) {
+        filePaths.push(`${req.files['image3'][0].filename}`);
+    }
+
+    res.json({ filePaths });
+});
+
+// Экспорт функции
+exports.api = functions.https.onRequest(app);
