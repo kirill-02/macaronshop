434cec3bb803f981d6c76f885b5f08f9d6dc5e72
kirill-02 <136984088+kirill-02@users.noreply.github.com>
2025-04-08 20:28:06 +0400

    update index.js,server.js, moderator.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '@/view/HomeView.vue';
 import AuthView from '@/view/AuthView.vue';
 import CabinetView from '@/view/CabinetView.vue';
@@ -205,7 +205,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(), // Измените на createWebHistory
+    history: createWebHashHistory(),
     routes,
 });
 
